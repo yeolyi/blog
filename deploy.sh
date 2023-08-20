13eaa517411396792fffb0c57f41854c05465d29
Yeolyi <yeolyi1310@gmail.com>
2023-08-20 15:18:53 +0900

    docker-compose 수정

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@ ssh -i "~/Developer/blog.pem" ubuntu@ec2-43-200-204-95.ap-northeast-2.compute.am
 scp -i "~/Developer/blog.pem" docker-compose.yml ubuntu@ec2-43-200-204-95.ap-northeast-2.compute.amazonaws.com:~/blog/docker-compose.yml
 scp -i "~/Developer/blog.pem" caddy/Caddyfile ubuntu@ec2-43-200-204-95.ap-northeast-2.compute.amazonaws.com:~/blog/caddy/Caddyfile
 
-docker build --platform linux/amd64 -t yeolyi1310/blog .
+timestamp=$(date +%Y%m%d%H%M%S)
+docker build --platform linux/amd64 -t yeolyi1310/blog:latest .
 docker push yeolyi1310/blog
 
 ssh -i "~/Developer/blog.pem" ubuntu@ec2-43-200-204-95.ap-northeast-2.compute.amazonaws.com -t "
