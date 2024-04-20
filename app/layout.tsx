a4c385ffef6c01c3e8128be2e64865fbad5bedc6
Yeolyi <yeolyi1310@gmail.com>
2024-04-20 22:55:04 +0900

    post: js 첨삭

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
         {/* https://play.tailwindcss.com/uj1vGACRJA?layout=preview 참조 */}
         <main className="flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-16 lg:py-24">
           <div className="relative w-full bg-white px-6 py-12 shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pb-28 lg:pt-16">
-            <div className="prose prose-slate mx-auto my-8 break-keep lg:prose-lg">{children}</div>
+            <div className="prose prose-slate mx-auto my-8 break-keep lg:prose-lg">
+              {children}
+            </div>
           </div>
         </main>
       </body>
