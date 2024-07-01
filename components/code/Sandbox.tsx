56eaa67510eddab88339f10e747777a2dc364da9
Yeolyi <yeolyi1310@gmail.com>
2024-07-01 19:56:42 +0900

    refactor: Sandbox hook

--- components/code/Sandbox.tsx
+++ components/code/Sandbox.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import CodeEditor from './CodeEditor';
 import Console from './Console';
 import { Log } from './type';
 import { createSrcDoc } from './src/createSrcDoc';
-import RefreshButton from './RefreshButton';
 
 export type SandboxOptions = {
   type: 'js' | 'babel' | 'html';
@@ -24,30 +23,57 @@ export default function Sandbox({
   code: string;
   options: SandboxOptions;
 }) {
-  const [code, setCode] = useState(_code);
-  const [srcdoc, setSrcdoc] = useState('');
-
   const [iframe, setIframe] = useState<HTMLIFrameElement | null>(null);
+  const { logList, reset } = useIframeListener(iframe, options.type);
+  const { code, setCode, srcdoc, iframeKey } = useDebouncedSrcDoc(
+    _code,
+    options.type,
+    reset,
+  );
+
+  let showConsole =
+    !options.executeDisabled &&
+    !(options.type === 'html' && logList.length === 0);
+
+  return (
+    <>
+      <div className="relative flex flex-col gap-2">
+        <CodeEditor
+          code={code}
+          setCode={setCode}
+          language={options.type === 'html' ? 'xml' : 'js'}
+          noneditable={options.editDisabled}
+        />
+        {options.type === 'html' && (
+          <iframe
+            key={iframeKey}
+            sandbox="allow-scripts"
+            className="min-h-[50px] resize-y bg-white shadow"
+            style={{ height: 0 }}
+            ref={(ref) => setIframe(ref)}
+            srcDoc={srcdoc}
+          />
+        )}
+        {showConsole && (
+          <Console logList={logList} expandedDefault={options.logExpanded} />
+        )}
+      </div>
+      {options.type !== 'html' && (
+        // https://velog.io/@younyikim/React%EC%97%90%EC%84%9C-Iframe-%EC%82%AC%EC%9A%A9%EC%8B%9C-%EB%92%A4%EB%A1%9C%EA%B0%80%EA%B8%B0%EA%B0%80-%EB%90%98%EC%A7%80-%EC%95%8A%EB%8A%94-%EB%B2%84%EA%B7%B8
+        <iframe
+          key={iframeKey}
+          sandbox="allow-scripts"
+          className="h-0 w-0"
+          ref={(ref) => setIframe(ref)}
+          srcDoc={srcdoc}
+        />
+      )}
+    </>
+  );
+}
+
+let useIframeListener = (iframe: HTMLIFrameElement | null, type: CodeType) => {
   const [logList, setLogList] = useState<Log[]>([]);
-
-  // ugly hack
-  // TODO: fix
-  const [tmp, setTmp] = useState(0);
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'development') setCode(_code);
-  }, [_code]);
-
-  let runCode = useCallback(() => {
-    if (iframe === null) return;
-    setSrcdoc(createSrcDoc(code, options.type));
-  }, [code, iframe, options.type]);
-
-  let refresh = () => {
-    setLogList([]);
-    setTmp((x) => x + 1);
-    runCode();
-  };
 
   useEffect(() => {
     if (iframe === null) return;
@@ -60,68 +86,47 @@ export default function Sandbox({
 
       let data = e.data;
       if (data.type === 'height') {
-        if (options.type === 'html') iframe.style.height = data.data;
+        if (type === 'html') iframe.style.height = data.data;
       } else {
         setLogList((list) => [...list, e.data]);
       }
     };
 
     addEventListener('message', handleMessage);
-    let id = setTimeout(runCode, 800);
+    return () => removeEventListener('message', handleMessage);
+  }, [iframe, type]);
 
-    return () => {
-      removeEventListener('message', handleMessage);
-      clearTimeout(id);
-    };
-  }, [code, iframe, options.type, runCode]);
+  let reset = useCallback(() => {
+    setLogList([]);
+  }, []);
 
-  let showRefresh =
-    !options.executeDisabled &&
-    !options.editDisabled &&
-    !options.refreshDisabled;
+  return { logList, reset };
+};
 
-  let showConsole =
-    !options.executeDisabled &&
-    !(options.type === 'html' && logList.length === 0);
+let useDebouncedSrcDoc = (
+  _code: string,
+  type: CodeType,
+  onWait: () => void,
+) => {
+  const [code, setCode] = useState(_code);
+  const [srcdoc, setSrcdoc] = useState('');
+  const iframeKey = useRef(0);
 
-  return (
-    <>
-      <div className="relative flex flex-col gap-2">
-        <CodeEditor
-          code={code}
-          setCode={setCode}
-          language={getCodeEditorLang(options.type)}
-          noneditable={options.editDisabled}
-        />
-        {showRefresh && <RefreshButton refresh={refresh} />}
-        {options.type === 'html' && (
-          <iframe
-            sandbox="allow-scripts"
-            className="min-h-[50px] resize-y bg-white shadow"
-            style={{ height: 0 }}
-            ref={(ref) => setIframe(ref)}
-            srcDoc={srcdoc}
-            key={srcdoc + tmp}
-          />
-        )}
-        {showConsole && (
-          <Console logList={logList} expandedDefault={options.logExpanded} />
-        )}
-      </div>
-      {options.type !== 'html' && (
-        // https://velog.io/@younyikim/React%EC%97%90%EC%84%9C-Iframe-%EC%82%AC%EC%9A%A9%EC%8B%9C-%EB%92%A4%EB%A1%9C%EA%B0%80%EA%B8%B0%EA%B0%80-%EB%90%98%EC%A7%80-%EC%95%8A%EB%8A%94-%EB%B2%84%EA%B7%B8
-        <iframe
-          sandbox="allow-scripts"
-          className="h-0 w-0"
-          ref={(ref) => setIframe(ref)}
-          srcDoc={srcdoc}
-          key={srcdoc + tmp}
-        />
-      )}
-    </>
-  );
-}
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') setCode(_code);
+  }, [_code]);
 
-let getCodeEditorLang = (type: CodeType) => {
-  return type === 'html' ? 'xml' : 'js';
+  useEffect(() => {
+    onWait();
+    setSrcdoc(createSrcDoc('', type));
+
+    let id = setTimeout(() => {
+      setSrcdoc(createSrcDoc(code, type));
+      iframeKey.current++;
+    }, 800);
+
+    return () => clearTimeout(id);
+  }, [code, onWait, type]);
+
+  return { code, setCode, srcdoc, iframeKey: iframeKey.current };
 };
