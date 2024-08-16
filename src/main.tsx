46a11383584de132e89ce5ad4f8c54ae9c79435c
leo.yi <leo.yi@kakaocorp.com>
2024-08-17 02:14:11 +0900

    web api 글들을 dynamic import로 불러옴

--- src/main.tsx
+++ src/main.tsx
@@ -9,18 +9,10 @@ import { lazy, Suspense } from 'react';
 import CodeBlock from './components/code/CodeBlock';
 import PostLayout from '@/components/layout/PostLayout';
 import { postCellProps } from '@/mdx/post/preview';
+import { webapiPreview } from '@/mdx/webapi/preview';
 
-let JSPost = ({ fileName }: { fileName: string }) => {
-  const Mdx = lazy(() => import(`./mdx/js/${fileName}.mdx`));
-  return (
-    <Suspense>
-      <Mdx components={{ pre: CodeBlock }} />
-    </Suspense>
-  );
-};
-
-let Post = ({ fileName }: { fileName: string }) => {
-  const Mdx = lazy(() => import(`./mdx/post/${fileName}.mdx`));
+let Post = ({ type, fileName }: { type: string; fileName: string }) => {
+  const Mdx = lazy(() => import(`./mdx/${type}/${fileName}.mdx`));
   return (
     <Suspense>
       <Mdx components={{ pre: CodeBlock }} />
@@ -38,15 +30,23 @@ const router = createBrowserRouter([
     element: <PostLayout discussionNumber={2} />,
     children: jsPostPreview.map(({ fileName }) => ({
       path: fileName,
-      element: <JSPost fileName={fileName} />,
+      element: <Post type="js" fileName={fileName} />,
+    })),
+  },
+  {
+    path: '/webapi',
+    element: <PostLayout />,
+    children: webapiPreview.map(({ fileName }) => ({
+      path: fileName,
+      element: <Post type="webapi" fileName={fileName} />,
     })),
   },
   {
     path: '/post',
-    element: <PostLayout discussionNumber={2} />,
+    element: <PostLayout />,
     children: postCellProps.map(({ fileName }) => ({
       path: fileName,
-      element: <Post fileName={fileName} />,
+      element: <Post type="post" fileName={fileName} />,
     })),
   },
 ]);
--- src/routes/main/components/WebCarousel.tsx
+++ src/routes/main/components/WebCarousel.tsx
@@ -1,6 +1,7 @@
 import Sandbox from '@/components/code/sandbox/Sandbox';
 import { Carousel } from './carousel/Carousel';
 import { Tile, TileProps } from './carousel/Tile';
+import { webapiPreview } from '@/mdx/webapi/preview';
 
 export let WebCarousel = () => {
   return (
@@ -12,126 +13,13 @@ export let WebCarousel = () => {
   );
 };
 
-let webTileProps: TileProps[] = [
-  {
-    title: 'Web API 기초',
-    description: '브라우저에서 JS가 어떤 과정으로 실행되는지 배웠습니다',
-    href: '/webapi/basic',
-    code: `<script>
-  setInterval(() => {
-    let str = new Date().toLocaleTimeString();
-    document.body.innerText = str;
-  }, 1000);
-</script>`,
-  },
-  {
-    title: '이벤트',
-    description:
-      '브라우저에서 이벤트가 어떻게 발생되고 전파되는지 공부했습니다.',
-    href: '/webapi/event',
-    code: `<p></p>
-<script>
-  let p = document.querySelector('p');
-  let cnt = 0;
-  addEventListener('click', () => {
-    p.innerText = ++cnt;
-  });
-</script>`,
-  },
-  {
-    title: 'Document 조작',
-    description: 'JS로 웹페이지의 내용을 바꾸는 방법을 공부했습니다.',
-    href: '/webapi/document',
-    code: `<p></p>
-<script>
-  let p = document.querySelector('p');
-  p.innerText = "Hello, World!";
-</script>`,
-  },
-  {
-    title: 'CSS 조작',
-    description: 'JS로 CSS 스타일을 바꾸는 방법을 공부했습니다',
-    href: '/webapi/css',
-    code: `<p>Hello!</p>
-<script>
-  let p = document.querySelector('p');
-  p.style.color = 'royalblue'
-</script>`,
-  },
-  {
-    title: '좌표와 스크롤',
-    description:
-      '요소의 크기와 위치, 스크롤을 알고 조작하는 법을 공부했습니다.',
-    href: '/webapi/geometry',
-    code: `<script>
-  addEventListener("click", (e) => {
-    console.log(e.offsetX, e.offsetY);
-    console.log(e.screenX, e.screenY);
-  });
-</script>`,
-  },
-  {
-    title: '컴포넌트',
-    description: '네이티브로 컴포넌트를 구현하는 방법을 공부했습니다',
-    href: '/webapi/component',
-    code: `<div>light DOM</div>
-<script>
-  let shadowHost = document.querySelector('div');
-  let shadowRoot = shadowHost.attachShadow({ mode: 'open' });
-  shadowRoot.innerHTML = '<p>shadow DOM <slot></slot></p>'
-</script>`,
-  },
-  {
-    title: 'SVG',
-    description:
-      '해상도 상관없이 깔끔하게 렌더링되는 이미지 포맷을 공부했습니다',
-    href: '/webapi/svg',
-    code: `<svg version="1.1" width="50" height="50" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="25" cy="25" r="25" fill="black" />
-  <text x="25" y="32" font-size="18" text-anchor="middle" fill="white">
-    SVG
-  </text>
-</svg>`,
-  },
-  {
-    title: 'Navigation',
-    description:
-      '히스토리 관련된 API들과 함께 pushState()로 상태를 관리하는 방법을 공부했습니다.',
-    href: '/webapi/navigation',
-    code: `<button>click me</button>
-<script>
-  document.querySelector('button').addEventListener('click', () => {
-    location = 'https://yeolyi.com';
-  });
-</script>`,
-  },
-  {
-    title: 'Network',
-    description:
-      '자바스크립트로 직접 네트워크 요청을 보내는 방법을 공부했습니다.',
-    href: '/webapi/network',
-    code: `<script>
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(resp => resp.json())
-    .then(json => console.log(json.title))
-</script>`,
-  },
-  {
-    title: 'Storage',
-    description: '브라우저상에 데이터를 저장하는 다양한 방법을 공부했습니다.',
-    href: '/webapi/storage',
-  },
-  {
-    title: 'Worker',
-    description: '별개의 스레드로 JS 코드를 실행시키는 방법을 공부했습니다.',
-    href: '/webapi/worker',
-  },
-].map((x) => ({
+let webTileProps: TileProps[] = webapiPreview.map((x) => ({
   ...x,
-  children: x.code && (
+  url: `/webapi/${x.fileName}`,
+  children: x.exampleCode && (
     <Sandbox
       presetName="html"
-      code={x.code}
+      code={x.exampleCode}
       norefresh
       iframeHeight={70}
       consoleFit
