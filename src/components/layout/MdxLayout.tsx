4b5a0881c62ca5a2a171582f275539a0f8894e4f
leo.yi <leo.yi@kakaocorp.com>
2024-08-18 23:37:15 +0900

    Suspense fallback 개선

--- src/components/layout/MdxLayout.tsx
+++ src/components/layout/MdxLayout.tsx
@@ -16,7 +16,13 @@ export default function MdxLayout({
         <div className="w-full bg-white py-[100px] md:py-[120px]">
           <article className="prose prose-slate mx-auto w-[87.5%] lg:prose-lg prose-h1:text-[32px] max-md:max-w-[414px] md:w-[83.33%] md:prose-h1:text-[40px] lg:w-[80%] lg:prose-h1:text-[48px]">
             <Island />
-            <Suspense fallback={<div className="h-full w-full"></div>}>
+            <Suspense
+              fallback={
+                <div className="h-[100vh] w-full">
+                  <h1 className="text-neutral-400">불러오는 중...</h1>
+                </div>
+              }
+            >
               {children}
             </Suspense>
             <div className="my-16 h-[1px] w-full bg-neutral-300" />
