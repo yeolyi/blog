8bdc7817c166f85a50f6ddc83e41bdb4140037fd
leo.yi <leo.yi@kakaocorp.com>
2024-09-22 19:51:33 +0900

    MdxLayout에서 이미지 유무에 따라 pt 분기처리

--- client/pages/[mdx]/index.tsx
+++ client/pages/[mdx]/index.tsx
@@ -35,7 +35,9 @@ export default function MdxLayout({
             }}
           />
         )}
-        <div className="w-full bg-white pb-[100px] pt-[50px] md:pb-[120px] md:pt-[60px]">
+        <div
+          className={`w-full bg-white pb-[100px] ${mdxPage.src ? 'pt-[50px] md:pt-[60px]' : 'pt-[100px] md:pt-[120px]'}`}
+        >
           <article className="prose prose-slate mx-auto w-[87.5%] lg:prose-lg prose-h1:text-[32px] max-md:max-w-[414px] md:w-[83.33%] md:prose-h1:text-[40px] lg:w-[80%] lg:prose-h1:text-[48px]">
             <Island />
             <Suspense
