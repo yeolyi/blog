e644cdb99618e5af91b35cfa2ca896ec3a545691
leo.yi <leo.yi@kakaocorp.com>
2025-04-20 22:24:18 +0900

    홈페이지 소개 섹션 다국어 지원 추가
    - `page.tsx`에서 `next-intl`의 `useTranslations` 훅을 사용하여 소개 문구를 다국어로 처리.
    - `messages/en.json` 및 `messages/ko.json`에 홈페이지 소개 메시지 추가 및 링크 처리.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,13 @@
 import PostList from '@/app/[locale]/components/Post';
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import me from './assets/me.jpg';
 
 export default function Home() {
+  const t = useTranslations('HomePage');
+
   return (
     <div className="max-w-2xl mx-auto mt-24 mb-[30vh] px-4 flex flex-col gap-16">
       <div className="relative w-full aspect-square mx-auto">
@@ -25,28 +28,32 @@ export default function Home() {
         </div>
       </div>
       <p className="text-xl font-semibold text-white break-keep">
-        안녕하세요👋 개발자 이성열입니다.{' '}
-        <Link
-          href="https://cse.snu.ac.kr"
-          className="text-white hover:text-black hover:bg-white underline active:text-black active:bg-white"
-        >
-          서울대 컴공 홈페이지
-        </Link>{' '}
-        리뉴얼 프로젝트에 프론트엔드 개발자로 참여했습니다. 지금은 카카오에서{' '}
-        <Link
-          href="https://edoc.kakao.com/desktop"
-          className="text-white hover:text-black hover:bg-white underline active:text-black active:bg-white"
-        >
-          전자문서
-        </Link>
-        를 개발합니다.{' '}
-        <Link
-          href="https://www.instagram.com/yeol.dev"
-          className="text-white hover:text-black hover:bg-white underline active:text-black active:bg-white"
-        >
-          인스타그램
-        </Link>
-        에는 개발자를 위한 컨텐츠를 공유해요.
+        {t.rich('bio', {
+          snuLink: (chunks) => (
+            <Link
+              href="https://cse.snu.ac.kr"
+              className="text-white hover:text-black hover:bg-white underline active:text-black active:bg-white"
+            >
+              {chunks}
+            </Link>
+          ),
+          edocLink: (chunks) => (
+            <Link
+              href="https://edoc.kakao.com/desktop"
+              className="text-white hover:text-black hover:bg-white underline active:text-black active:bg-white"
+            >
+              {chunks}
+            </Link>
+          ),
+          instagramLink: (chunks) => (
+            <Link
+              href="https://www.instagram.com/yeol.dev"
+              className="text-white hover:text-black hover:bg-white underline active:text-black active:bg-white"
+            >
+              {chunks}
+            </Link>
+          ),
+        })}
       </p>
 
       <Suspense>
