e4a4280ff3e2c7d2e5467bc182c748a064f26f7b
leo.yi <leo.yi@kakaocorp.com>
2025-05-08 22:20:05 +0900

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,3 +1,4 @@
+import { generateStaticParams as generateStaticCsParams } from '@/app/[locale]/(mdx)/cs/[id]/page';
 import { generateStaticParams as generateStaticPostParams } from '@/app/[locale]/(mdx)/post/[id]/page';
 import { routing } from '@/i18n/routing';
 import type { MetadataRoute } from 'next';
@@ -6,6 +7,8 @@ const BASE_URL = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const postStaticParams = await generateStaticPostParams();
+  const csStaticParams = await generateStaticCsParams();
+  const staticParams = [...postStaticParams, ...csStaticParams];
 
   return [
     {
@@ -14,7 +17,25 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'weekly',
       priority: 1,
     },
-    ...postStaticParams.map(({ id, locale }) => {
+    {
+      url: `${BASE_URL}/en`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/cs`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/en/cs`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    ...staticParams.map(({ id, locale }) => {
       const localeUrl = locale === routing.defaultLocale ? '' : `${locale}/`;
       return {
         url: `${BASE_URL}/${localeUrl}post/${id}`,
