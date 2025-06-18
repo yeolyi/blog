c8db38dd1a11e4f14d9d705faec3c3f9d6b31f37
leo.yi <leo.yi@kakaocorp.com>
2025-06-18 15:40:15 +0900

    react suspense

---
+++ mdx/react/suspense/Demo.jsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Suspense, useState } from 'react';
+
+const getResource = (data, delay = 1000) => ({
+  _data: null,
+  _promise: null,
+  status: 'pending',
+  get data() {
+    if (this.status === 'ready') {
+      return this._data;
+    }
+
+    if (this._promise == null) {
+      this._promise = new Promise((resolve) => {
+        setTimeout(() => {
+          this._data = data;
+          this.status = 'ready';
+          resolve();
+        }, delay);
+      });
+    }
+    throw this._promise;
+  },
+});
+
+const Child = ({ resource }) => {
+  return <div>{JSON.stringify(resource)}</div>;
+};
+
+export default function SuspenseDemo() {
+  const [resource, setResource] = useState(null);
+  return (
+    <div className="app">
+      <button
+        type="button"
+        onClick={() => {
+          setResource(getResource('JSer'));
+        }}
+      >
+        start
+      </button>
+      <Suspense fallback={<p>loading...</p>}>
+        <Child resource={resource} />
+      </Suspense>
+    </div>
+  );
+}
---
+++ mdx/react/suspense/Use.jsx
@@ -0,0 +1,17 @@
+import { Suspense, use } from 'react';
+
+const fetch = () =>
+  new Promise((resolve) => setTimeout(() => resolve('data'), 1000));
+
+function Child() {
+  const data = use(fetch());
+  return <div>{data}</div>;
+}
+
+export default function Use() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Child />
+    </Suspense>
+  );
+}
